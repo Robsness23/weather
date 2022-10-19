f68cac9dcb23d32b4c176ccc7945aed3dba5192d
robynsm <robynamysmith92@gmail.com>
2022-10-19 17:23:16 +0100

    Added Github URL and Portfolio URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div>
+    <h1 className="App">Weather App</h1>
+    <footer>
+      Coded by <a 
+      href="https://www.robyn-smith.co.uk/"
+      target="_blank"
+      rel="noreferrer">
+      Robyn Smith</a> <br />
+      <a 
+      href="https://github.com/Robsness23/weather" 
+      target="_blank" 
+      rel="noreferrer">
+        Open-sourced on GitHub
+      </a>
+    </footer>
+    </div>
   );
 }
 
