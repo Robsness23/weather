fb557772fd385d906a8a4959389914ad3f7ab642
robynsm <robynamysmith92@gmail.com>
2022-10-20 21:32:22 +0100

    Added Weather component with City, time and weather condition

---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Weather.css'
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Belfast</h1>
+      <ul>
+        <li>Thursday, 21:30</li>
+        <li>Rain</li>
+      </ul>
+      Hello from Weather
+    </div>
+  )
+}
