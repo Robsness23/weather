391ba2cef555be7611ce044a5f709d63ae34f332
robynsm <robynamysmith92@gmail.com>
2022-10-20 21:22:00 +0100

    Implemented Bootstrap and removed WebVitals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
 
 export default function App() {
   return (
-    <div>
-    <h1 className="App">Weather App</h1>
+    <div className="App">
+      <div className="container">
+    <h1>Weather App</h1>
     <footer>
       Coded by <a 
       href="https://www.robyn-smith.co.uk/"
@@ -17,6 +19,7 @@ export default function App() {
         Open-sourced on GitHub
       </a>
     </footer>
+    </div>
     </div>
   );
 }
