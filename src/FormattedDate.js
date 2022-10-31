189c6e4d2bbe331c47aaa5e7b034ab9f564c9698
robynsm <robynamysmith92@gmail.com>
2022-10-31 21:08:28 +0000

    Cleaned up FormatttedDate component

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 export default function FormattedDate(props) {
   console.log(props.date)
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let days = 
+  [
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+  ]
   let day = days[props.date.getDay()]
   let hours = props.date.getHours()
   if (hours < 10) {
@@ -13,8 +22,9 @@ export default function FormattedDate(props) {
     minutes = `0${minutes}`
 
   }
-  return <div>
+  return (<div>
     {day} {hours}:{minutes}
 
   </div>
+  )
 }
